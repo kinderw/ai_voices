9782fec391da36cdfb113aa2c25335c83774772b
kinderw <tibsnow65@gmail.com>
2023-02-04 13:46:28 -0500

    added re

--- cloudtrail/service_events.py
+++ cloudtrail/service_events.py
@@ -1,5 +1,6 @@
 import boto3
 from datetime import datetime
+import re
 
 def get_ecs_task_failure_stop_codes(service_name, start_time, end_time):
     client = boto3.client("logs")
@@ -14,12 +15,15 @@ def get_ecs_task_failure_stop_codes(service_name, start_time, end_time):
     results = []
     for event in response['events']:
         message = event['message']
-        if "Task failed to start" in message:
-            task_id, stop_code = message.split(" stopped with stop code ")
-            task_id = task_id.split(" ")[-1]
-            results.append({
-                "task_id": task_id,
-                "stop_code": stop_code
-            })
+        if "TaskFailedToStart" in message or "EssentialContainerExited" in message:
+            task_id_match = re.search(r"task (\w+)", message)
+            stop_code_match = re.search(r"stopCode ([\w:]+)", message)
+            if task_id_match and stop_code_match:
+                task_id = task_id_match.group(1)
+                stop_code = stop_code_match.group(1)
+                results.append({
+                    "task_id": task_id,
+                    "stop_code": stop_code
+                })
 
     return results
